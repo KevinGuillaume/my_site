20aa37645262bb872cbfd4718a051d81f4a2bc1c
Kevin <keving3492@gmail.com>
2022-06-19 23:31:36 -0500

    Cleaaring old content

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -28,25 +28,7 @@ function AboutMe() {
                             </div>
                             <div className="line"> </div>
                             <div className="bio">
-                                Thanks for checking out my site. 
-                                It started off completely different, then slowly transitioned to what it is now.
-                                There are still different things I want to change or fix, but it has been cool just playing around with it.
-                                <br />
-                                <br/>
-                                <b>About me</b>
                                 
-                                <br/>
-                                I started coding around my high school days, but got a lot more into it during my senior year of high school.
-                                Since then I've been interested in creating projects and interacting with different frameworks,languages, or tools.
-                                I'm trying to graduate college a year early so that I can have time to really understand and learn concepts deeply.
-                                It's always a great and rewarding feeling seeing something you envision come to life with just lines of codes. 
-                                <br />
-                                <br />
-                                When I'm not coding you can find me playing basketball with some friends.
-                                I grew up playing basketball and play now recreationally or sometimes competetivly in some men's leagues.
-                                Blockchain has also been something that has caught my eye. I like to read up on current topics surround the web3 world.
-                                Oh, and I'm also a big fan of video games and I'm always looking for some new recommandations so send them my way. 
-
                                 
                                 
                             </div>
